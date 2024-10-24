e005a939f6e923291a9e85ffaaad0bb41590e504
jaykkoo <adrien.gutleben@gmail.com>
2024-10-24 23:57:48 +0200

    change settings

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.164.227.130"]/' /home/ubuntu/test-deploy/conf/settings.py
+# sed -i 's/\[]/\["54.164.227.130"]/' /home/ubuntu/test-deploy/conf/settings.py
 
-sudo cd /home/ubuntu/test-deploy
-touch filename.txt
+# sudo cd /home/ubuntu/test-deploy
+# touch filename.txt
 
-python manage.py migrate 
-python manage.py makemigrations     
-python manage.py collectstatic
-sudo service gunicorn restart
-sudo service nginx restart
+# python manage.py migrate 
+# python manage.py makemigrations     
+# python manage.py collectstatic
+# sudo service gunicorn restart
+# sudo service nginx restart
 #sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
@@ -22,3 +22,26 @@ sudo service nginx restart
 # Restart:
 #systemctl restart gunicorn
 #sudo systemctl status nginx
+
+#!/bin/ba
+sed -i 's/\[]/\["54.164.227.130"]/' /home/ubuntu/test-deploy/conf/settings.py
+
+LOGFILE="/home/ubuntu/start_app.log"
+echo "Starting application at $(date)" >> $LOGFILE
+
+# Run Django management commands
+cd /home/ubuntu/test-deploy
+/home/ubuntu/venv/bin/python manage.py migrate >> $LOGFILE 2>&1
+/home/ubuntu/venv/bin/python manage.py collectstatic --noinput >> $LOGFILE 2>&1
+
+# Restart Gunicorn
+echo "Restarting Gunicorn..." >> $LOGFILE
+sudo service gunicorn restart >> $LOGFILE 2>&1
+
+# Restart Nginx
+echo "Restarting Nginx..." >> $LOGFILE
+sudo service nginx restart >> $LOGFILE 2>&1
+
+# Confirm completion
+echo "Application started successfully at $(date)" >> $LOGFILE
+
